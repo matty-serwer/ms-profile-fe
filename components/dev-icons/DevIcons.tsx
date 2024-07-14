3a24ab2904bf08bcae3f2c6f94ab6cba3996ecbb
Matty Serwer <Matthew.Serwer@gmail.com>
2024-07-14 17:09:10 -0400

    Sets up application with boilerplate 'pages'. Sets text color variables. Moves devicon page to a component called dev-icons

---
+++ components/dev-icons/DevIcons.tsx
@@ -0,0 +1,45 @@
+import styles from "@/components/dev-icons/DevIcons.module.css";
+
+const DevIcons = () => {
+  return (
+    <div id="dev-icons">
+      <div className={styles.iconsGradientOne}>
+        <div id="languages" className={styles.iconRowLarge}>
+          <i className="devicon-java-plain"/>
+          <i className="devicon-javascript-plain"/>
+          <i className="devicon-typescript-plain"/>
+          <i className="devicon-python-plain"/>
+          <i className="devicon-html5-plain"/>
+          <i className="devicon-css3-plain"/>
+          {/*<i className="devicon-bash-plain" />*/}
+        </div>
+        <div id="frameworks" className={styles.iconRow}>
+          <i className="devicon-react-original"/>
+          <i className="devicon-redux-original"/>
+          <i className="devicon-nextjs-plain"/>
+          <i className="devicon-angularjs-plain"/>
+          <i className="devicon-nodejs-plain"/>
+          <i className="devicon-express-original"/>
+          <i className="devicon-spring-original"/>
+        </div>
+        <div id="software" className={styles.iconRowSmall}>
+          <i className="devicon-amazonwebservices-plain"/>
+          <i className="devicon-dynamodb-plain"/>
+          <i className="devicon-googlecloud-plain"/>
+          <i className="devicon-heroku-original"/>
+          <i className="devicon-mongodb-plain"/>
+          <i className="devicon-postgresql-plain"/>
+          <i className="devicon-docker-plain"/>
+          <i className="devicon-sass-plain"/>
+          <i className="devicon-less-plain-wordmark"/>
+          <i className="devicon-bootstrap-plain"/>
+          <i className="devicon-materialui-plain"/>
+          <i className="devicon-angularmaterial-plain"/>
+          <i className="devicon-tailwindcss-plain"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DevIcons;
---
+++ components/home/HomeHero.tsx
@@ -0,0 +1,9 @@
+const HomeHero = () => {
+  return (
+    <div>
+      <h1>HomeHero</h1>
+    </div>
+  );
+};
+
+export default HomeHero;
